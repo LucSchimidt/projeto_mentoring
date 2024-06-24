3050cade873315670dd1e46b4361bbf8e03b0ee8
Lucas Schimidt <llourencoschimidt@gmail.com>
2024-06-24 16:41:53 -0300

    Projeto totalmente desenvolvido

---
+++ script.js
@@ -0,0 +1,24 @@
+function gotoMain() {
+
+    window.location.href = "./main.html"
+}
+
+function gotoEvolution() {
+
+    window.location.href = "./evolution.html"
+}
+
+function gotoApiration() {
+
+    window.location.href = "./aspirations.html"
+}
+
+function gotoGithub() {
+
+    window.open("https://github.com/LucSchimidt")
+}
+
+function gotoLinkedin() {
+
+    window.open("https://www.linkedin.com/in/lucas-schimidt/")
+}
